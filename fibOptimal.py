a32ce4b50e98eb5cc58c72680f4a755b58e6d523
Patrick Niyongabo <patrickniyongabo@gmail.com>
2017-03-08 12:51:08 -0600

    added a method using recursion with memoization

--- fibOptimal.py
+++ fibOptimal.py
@@ -1,17 +1,37 @@
+# Fibonacci #1: naive recursive algorithm
 def fibSlow(n):
     if n <= 1:
         return n 
     else:
         return fibSlow(n-1) + fibSlow(n-1)
+#print (fibSlow(30)) # this is very slow        
 
+
+        
+# Fibonacci #2: explicitly filling in a table with a loop
 def fibFast1(n):
     fibs = [0] * (n+1) # 0th to the nth elt
     fibs[1] = 1 # fibs[0] = 0 is already set
     for i in range(2, n+1):
         fibs[i] = fibs[i-1] + fibs[i-2]
     return fibs[i]
-        
-#print (fibSlow(30)) # this is very slow
-print (fibFast1(40)) # way faster !!!
+print (fibFast1(40)) # faster !!!
+
+
+
+# Fibonacci #3: recursion with memoization
+# Keep a global table to remember the results of fib3
+fibtable = [0,1]
+def fibFast2(n):
+# Expand the table as necessary
+    while len(fibtable) < n+1:
+        fibtable.append(-1)
+# Fill in the table recursively (only if necessary)
+    if fibtable[n] == -1:
+        fibtable[n] = fibFast2(n-1) + fibFast2(n-2)
+    return fibtable[n]
+print (fibFast2(40)) # faster !!!
+    
+    
     
     
